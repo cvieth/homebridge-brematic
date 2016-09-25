3b7a6216011e022cfd1e2a40132a7cff4ae2e3f4
Christoph G. Vieth <christoph@vieth.me>
2016-09-25 13:17:09 +0200

    First version that is able to turn switches on

--- index.js
+++ index.js
@@ -1,59 +1,173 @@
-var Accessory, Service, Characteristic, UUIDGen;
+function encodeMessage(system, unit, state) {
+    var sA = 0;
+    var sG = 0;
 
-module.exports = function (homebridge) {
-    console.log("homebridge API version: " + homebridge.version);
+    var sRepeat = 15;
+    var sRepeat = 15;
+    var sPause = 5600;
+    var sTune = 350;
+    var sBaud = 25;
+    var sSpeed = 16;
 
-    // Accessory must be created from PlatformAccessory Constructor
-    Accessory = homebridge.platformAccessory;
+    var uSleep = 800000;
 
-    // Service and Characteristic are from hap-nodejs
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
+    var HEAD = "TXP:" + sA + "," + sG + "," + sRepeat + "," + sPause + "," + sTune + "," + sBaud + ",";
 
-    // For platform plugin to be considered as dynamic platform plugin,
-    // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-    homebridge.registerPlatform("homebridge-brematic", "Brematic", brematicPlatform, true);
+    var TAIL = ",1,1," + sSpeed + ",;";
+    //var TAIL = ",3,1," + sSpeed + ",;";
+    var AN = "1,3,1,3,3";
+    var AUS = "3,1,1,3,1";
 
-};
+    var bitLow = 1;
+    var bitHigh = 3;
 
+    var seqLow = bitHigh + "," + bitHigh + "," + bitLow + "," + bitLow + ",";
+    var seqHgh = bitHigh + "," + bitLow + "," + bitHigh + "," + bitLow + ",";
 
-function brematicPlatform(log, config, api) {
-    log("brematicPlatform Init");
-    var platform = this;
-    this.log = log;
-    this.config = config;
-    //this.accessories = [];
-
-    if (api) {
-        // Save the API object as plugin needs to register new accessory via this object.
-        this.api = api;
-
-        // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories
-        // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-        // Or start discover new accessories
-        this.api.on('didFinishLaunching', function () {
-            platform.log("DidFinishLaunching");
-        }.bind(this));
+    var msg = "";
+    var address = system;
+    for (var i = 0, len = address.length; i < len; i++) {
+        var bit = address[i];
+        if (bit == "0") {
+            msg = msg + seqLow;
+        } else {
+            msg = msg + seqHgh;
+        }
     }
+    var msgM = msg;
 
-    var devices = config['devices'];
+    var msg = "";
+    var address = unit;
+    for (var i = 0, len = address.length; i < len; i++) {
+        var bit = address[i];
+        if (bit == "0") {
+            msg = msg + seqLow;
+        } else {
+            msg = msg + seqHgh;
+        }
+    }
+    var msgU = msg;
 
-    this.log(devices);
+    if (state = "on") {
+        var message = HEAD + bitLow + "," + msgM + msgU + bitHigh + "," + AN + TAIL;
+    } else {
+        var message = HEAD + bitLow + "," + msgM + msgU + bitHigh + "," + AUS + TAIL;
+    }
+    return message;
 }
 
-brematicPlatform.prototype.configureAccessory = function (accessory) {
-    this.log("-> configureAccessory");
+var Service;
+var Characteristic;
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-brematic", "Brematic", brematicAccessory);
 };
-brematicPlatform.prototype.configurationRequestHandler = function (context, request, callback) {
-    this.log("-> configurationRequestHandler");
+
+function brematicAccessory(log, config) {
+    this.log = log;
+
+    this.log("Init...");
+
+    this.service = 'Switch';
+
+    // Fetch Configruration
+    this.name = config.name;
+    this.host = config.host;
+    this.port = config.port;
+    this.device = config.device;
+    this.systemCode = config.systemCode;
+    this.unitCode = config.unitCode;
+}
+
+/*
+ brematicAccessory.prototype.matchesString = function (match) {
+ if (this.exactMatch) {
+ return (match === this.onValue);
+ }
+ else {
+ return (match.indexOf(this.onValue) > -1);
+ }
+ }
+ */
+
+brematicAccessory.prototype.setState = function (powerOn, callback) {
+    var accessory = this;
+    var state = powerOn ? 'on' : 'off';
+    var prop = state + 'Command';
+    var command = accessory[prop];
+
+    accessory.log('Setting ' + accessory.name + ' to ' + state);
+
+    var message = encodeMessage(this.systemCode, this.unitCode, state);
+    var dgram = require('dgram');
+    var buffer = new Buffer(message);
+
+    var client = dgram.createSocket('udp4');
+    client.send(buffer, 0, buffer.length, accessory.port, accessory.host, function (err, bytes) {
+        if (err) throw err;
+        accessory.log('UDP message sent to ' + accessory.host + ':' + accessory.port + ' >> ' + message);
+        callback(null);
+        client.close();
+    });
+
+
+    /*
+     var stream = ssh(command, accessory.ssh);
+
+     stream.on('error', function (err) {
+     accessory.log('Error: ' + err);
+     callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
+     });
+
+     stream.on('finish', function () {
+     accessory.log('Set ' + accessory.name + ' to ' + state);
+     callback(null);
+     });
+     */
 };
-brematicPlatform.prototype.addAccessory = function (accessoryName) {
-    this.log("-> addAccessory");
+
+brematicAccessory.prototype.getState = function (callback) {
+    var accessory = this;
+
+    accessory.log('State of ' + accessory.name + ' fetching');
+    callback(null, true);
+    /*
+     var command = accessory['stateCommand'];
+
+     var stream = ssh(command, accessory.ssh);
+
+     stream.on('error', function (err) {
+     accessory.log('Error: ' + err);
+     callback(err || new Error('Error getting state of ' + accessory.name));
+     });
+
+     stream.on('data', function (data) {
+     var state = data.toString('utf-8').trim().toLowerCase();
+     accessory.log('State of ' + accessory.name + ' is: ' + state);
+     callback(null, accessory.matchesString(state));
+     });
+     */
 };
-brematicPlatform.prototype.updateAccessoriesReachability = function () {
-    this.log("-> updateAccessoriesReachability");
-};
-brematicPlatform.prototype.removeAccessory = function () {
-    this.log("-> removeAccessory");
+
+brematicAccessory.prototype.getServices = function () {
+    var accessory = this;
+
+    accessory.log(accessory.name + ' - getServices');
+
+
+    var informationService = new Service.AccessoryInformation();
+    var switchService = new Service.Switch(this.name);
+
+    informationService
+        .setCharacteristic(Characteristic.Manufacturer, 'Brennenstuhl')
+        .setCharacteristic(Characteristic.Model, this.device)
+        .setCharacteristic(Characteristic.SerialNumber, this.systemCode + '-' + this.unitCode);
+
+    var characteristic = switchService.getCharacteristic(Characteristic.On);
+    characteristic.on('set', this.setState.bind(this));
+    characteristic.on('get', this.getState.bind(this));
+
+    return [switchService];
 };
